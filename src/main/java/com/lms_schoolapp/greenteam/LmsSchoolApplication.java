fb87b154213d8e5de9f2dc764f07f71f7ff50d03
Eduardo Hernandez <eduardohralejandro@gmail.com>
2024-09-16 11:15:26 +0200

    Initial commit

---
+++ src/main/java/com/lms_schoolapp/greenteam/LmsSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.lms_schoolapp.greenteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lms_schoolapp/greenteam/LmsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms_schoolapp.greenteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
